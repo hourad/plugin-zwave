140572eeb2fc08a2dae96fc0f33a269fb7995edd
Loic <loicgevrey@gmail.com>
2015-03-15 11:59:03 +0100

    Fix update

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -57,9 +57,9 @@ function zwave_update() {
 		}
 		$zwave->save();
 	}
-	config::save('zwaveAddr1', config::byKey('zwaveAddr', 'zwave'), 'zwave');
-	config::save('zwavePort1', config::byKey('zwavePort', 'zwave'), 'zwave');
-	config::save('isOpenZwave1', config::byKey('isOpenZwave', 'zwave'), 'zwave');
+	config::save('zwaveAddr1', config::byKey('zwaveAddr1', 'zwave', config::byKey('zwaveAddr', 'zwave')), 'zwave');
+	config::save('zwavePort1', config::byKey('zwavePort1', 'zwave', config::byKey('zwavePort', 'zwave')), 'zwave');
+	config::save('isOpenZwave1', config::byKey('isOpenZwave1', 'zwave', config::byKey('isOpenZwave', 'zwave')), 'zwave');
 }
 
 function zwave_remove() {
